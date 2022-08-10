f58cb7cb76db52e6ebe823cdef6547f66bcc5cee
JE <wjd7dms7@naver.com>
2022-08-10 10:27:55 +0900

    Updates

---
+++ precache-manifest.481fe24ea917456be42d5e089056e899.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3679699a144033b533b",
+    "url": "/koke_React/static/css/main.ecae92d0.chunk.css"
+  },
+  {
+    "revision": "a3679699a144033b533b",
+    "url": "/koke_React/static/js/main.f9f21a65.chunk.js"
+  },
+  {
+    "revision": "90fc3be59bfac4505419",
+    "url": "/koke_React/static/js/runtime~main.3def8bca.js"
+  },
+  {
+    "revision": "5a7509eed02a6636fc43",
+    "url": "/koke_React/static/css/2.cbd38a4e.chunk.css"
+  },
+  {
+    "revision": "5a7509eed02a6636fc43",
+    "url": "/koke_React/static/js/2.3a66a262.chunk.js"
+  },
+  {
+    "revision": "7da80a4b2c1fc8dd018d",
+    "url": "/koke_React/static/js/3.e3b4af8e.chunk.js"
+  },
+  {
+    "revision": "fc759ca03663e6624fead8ded6a4530b",
+    "url": "/koke_React/static/media/icon_02.fc759ca0.png"
+  },
+  {
+    "revision": "aa357a0d59e15e268db7f03d06768211",
+    "url": "/koke_React/static/media/icon_03.aa357a0d.png"
+  },
+  {
+    "revision": "55d10a670693a006ad9ca2c3654136a8",
+    "url": "/koke_React/static/media/icon_04.55d10a67.png"
+  },
+  {
+    "revision": "48461edd33c95abd02ffb1132cc0a39f",
+    "url": "/koke_React/static/media/main_visual.48461edd.jpeg"
+  },
+  {
+    "revision": "a171e5878440f3bba748d61a9d495312",
+    "url": "/koke_React/static/media/main_banner.a171e587.jpg"
+  },
+  {
+    "revision": "ad35e38526bfa0309b2815238ce5b49b",
+    "url": "/koke_React/index.html"
+  }
+];
